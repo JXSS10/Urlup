35d5ebdedc4662e93582ef9713b19c63bf1ab517
JXSS10 <mrhetlr98@gmail.com>
2025-06-13 00:45:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 
 """ Bot credentials (Keep your credentials) """
 
-API_HASH = "0f3793daaf4d3905e55b0e44d8719cad"
-API_ID = 20028561
-BOT_TOKEN = "7384885023:AAExDXSDebiTgY42VGiSHBp4NxyAXH4u7pM"
+API_HASH = "90033959db857ba1eaf5b778f951e6ad"
+API_ID = 24629872
+BOT_TOKEN = "7277293137:AAEhI-01Fzd92vGuqo0rUYjb5yaH5JxuhDw"
 
 # config.py
 
