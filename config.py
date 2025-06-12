f9427b3397c05fa2874c6b8036f37badca724806
JXSS10 <mrhetlr98@gmail.com>
2025-06-13 00:08:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,24 +6,9 @@ import os
 
 API_HASH = "0f3793daaf4d3905e55b0e44d8719cad"
 API_ID = 20028561
-BOT_TOKEN = "7079327825:AAGao7LcQMRvVPKniYSGB06nyZcxhf5OfgI"
+BOT_TOKEN = "7384885023:AAExDXSDebiTgY42VGiSHBp4NxyAXH4u7pM"
 
-""" Default thumbnail path (Keep your default thumbnail) """
+# config.py
 
-DEFAULT_THUMBNAIL = "default_thumb.jpg"
 
-""" Force Subscribe Channel ID (Keep your channel ID) """
-
-FORCE_SUBSCRIBE_CHANNEL_ID = -1001483534821
-
-""" Admin user IDs (Keep your admin IDs) """
-
-ADMIN_IDS = [6169288210]
-
-""" Log channel ID (Keep your log channel ID) """
-
-LOG_CHANNEL_ID = -1002228859831
-
-""" #-- Daily Task Limit Variables --- (Keep these) """
-
-DAILY_TASK_LIMIT = 10
+COOKIES_FILE_PATH = "cookies.txt"
